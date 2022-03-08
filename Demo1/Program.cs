2a10a767e783ce24edf92c1be0b43bc3f79ac8fe
GET Academy <terje@getacademy.no>
2022-03-08 10:40:31 +0100

    Initial commit

---
+++ Demo1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Pause til 10:35
+                - dele opp i metoder, reshaper
+                - klasse
+                - namespace og using
+                - github
+            */
+
+            // verdityper
+            bool b = true;
+            float ff = 1f;
+            float f = 5.5f;
+            double d = 5.5;
+            decimal dd = 1000.7m;
+            int j = 5;
+            char c = 'a';
+
+            // referansetype
+            string s = "hello";
+
+            Console.WriteLine(args.Length);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Indeks {i} inneholder: {args[i]}");
+            }
+        }
+    }
+}
